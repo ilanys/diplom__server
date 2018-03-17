1c6f5396b5c8b7ca5e29eb650f42cda3efd5badf
Ilya <islesarenok@mail.ru>
2018-03-17 22:40:41 +0300

    Добавлены новости + переделаны работа с пользователем

--- ServerMarch/Form1.cs
+++ ServerMarch/Form1.cs
@@ -12,7 +12,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Data.SqlClient;
 using System.Threading;
-
+using System.Security.Cryptography;
 
 namespace ServerMarch
 {
@@ -23,6 +23,12 @@ namespace ServerMarch
             InitializeComponent();
         }
 
+        public string transform__password(string password)
+        {
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            return Encoding.UTF8.GetString(sha.ComputeHash(Encoding.UTF8.GetBytes(password)));
+        }
+
         string sql__connect(string command,bool otv,string text)
         {
             if (!otv)
@@ -31,7 +37,6 @@ namespace ServerMarch
                 SqlCommand sql__command;
                 using (sql__command = new SqlCommand(command, sql))
                 {
-                    sql__command.Connection.Open();
                     sql__command.ExecuteNonQuery();
                 }
                 sql.Close();
@@ -42,28 +47,57 @@ namespace ServerMarch
                 sql.Open();
                 string otev = "";
                 SqlCommand sql__command;
+                string JSON = "";
                 using (sql__command = new SqlCommand(command, sql))
                 {
                     SqlDataReader dr = sql__command.ExecuteReader();
                     while (dr.Read())
                     {
                         if(text=="Log")
-                            otev = dr["login"] + "$" + dr["password"];
+                            otev = dr["logins"].ToString();
                         if (text == "token")
-                            otev = dr["token"].ToString();
+                            otev = dr["logins"].ToString()+"$" + dr["email"].ToString() + "$"+dr["name"].ToString()+ "$" + dr["lastname"].ToString()+ "$" + dr["double__name"].ToString() + "$" + dr["token"].ToString()+"$"+dr["type"].ToString();
+                        if (text == "email")
+                            otev = dr["email"].ToString();
+                        if(text == "News")
+                        {
+                            JSON += dr["header"].ToString()+"%" + dr["body"] +"%"+ dr["picture"] +"=";
+                        }
+
                     }
+                }
+                
+                if(text=="News")
+                {
+                    sql.Close();
+                    return JSON;
                 }
                 sql.Close();
                 return otev;
             }
         }
 
+        public string add__token(string login, string password)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            md5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(password+login));
+            byte[] result = md5.Hash;
+            StringBuilder strBuilder = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                strBuilder.Append(result[i].ToString("x2"));
+            }
+            login = strBuilder.ToString();
+            return login;
+
+            return "";
+        }
 
 
         SqlConnection sql;
         Socket server;
         byte[] buffer;
-        byte[] reception;
+        byte[] reception=new byte[2048];
         IPEndPoint iPEnd;
         DateTime dat = new DateTime();
         void server__start()
@@ -82,23 +116,30 @@ namespace ServerMarch
                        switch (reception__string.Split('/')[1])
                        {
                           case "Add": {
-                                        string command = "use Test" + '\n' + "Select From dbo.[user] where dbo.[user].login='" + reception__string.Split('/')[2].Split('%')[0] + "'";
+                                        string command = "use Test" + '\n' + " Select logins from users__bd where logins='" + reception__string.Split('/')[2].Split('%')[0] + "'";
                                         string log__text = sql__connect(command, true, "Log");
-                                        if (log__text == "" || log__text == " ")
+                                        if (log__text != "")
                                         {
-                                            buffer = Encoding.UTF8.GetBytes("Ошибка 404 Пользователь нет в базе");
+                                            buffer = Encoding.UTF8.GetBytes("Error404");
                                             client.Send(buffer);
                                             return;
                                         }
 
 
-                                        command = "USE [Test]"+'\n';
-                                        command += "GO" + '\n';
-                                        command += "INSERT INTO[dbo].[user]"+'\n';
-                                        command += "VALUES" + '\n';
-                                        command += "('" + reception__string.Split('/')[2].Split('%')[0] + "', '" + reception__string.Split('/')[2].Split('%')[1] + "', '" + reception__string.Split('/')[2].Split('%')[2] + "','" + reception__string.Split('/')[2].Split('%')[3] + "',";
-                                        command += "'" + reception__string.Split('/')[2].Split('%')[4] + " ','" + "Null" + "','" + reception__string.Split('/')[2].Split('%')[6] + "')";
-                                        command += "GO";
+                                        command = "use Test" + '\n' + " Select logins from users__bd where email = '" + reception__string.Split('/')[4]+ "'";
+                                        log__text = sql__connect(command, true, "email");
+
+                                        if (log__text != "" )
+                                        {
+                                            buffer = Encoding.UTF8.GetBytes("Error405");
+                                            client.Send(buffer);
+                                            return;
+                                        }
+
+                                        command = "INSERT INTO[dbo].[users__bd] "+'\n';
+                                        command += " VALUES " + '\n';
+                                        command += "('" + reception__string.Split('/')[2] + "', '" + reception__string.Split('/')[3] + "' , '" + reception__string.Split('/')[4]+ "','" + reception__string.Split('/')[5]+ "',";
+                                        command += "' ',' ', "+"'"+add__token(reception__string.Split('/')[2],reception__string.Split('/')[3]) +"' , '"+reception__string.Split('/')[6]+"' )";
 
                                         string a= sql__connect(command,false,"");
 
@@ -125,24 +166,45 @@ namespace ServerMarch
 
                           case "IN":
                           {
-                                        
-                                        string command = "use Test" + '\n' + "Select From dbo.[user] where dbo.[user].login='" + reception__string.Split('/')[2].Split('%')[0] + "'";
+
+                                        string command = "use Test" + '\n' + " Select logins from users__bd where logins='" + reception__string.Split('/')[2].Split('%')[0] + "'";
                                         string log__text = sql__connect(command, true, "Log");
-                                        if(log__text==""||log__text==" ")
+                                        if (log__text == "" || log__text == " ")
                                         {
-                                            buffer = Encoding.UTF8.GetBytes("Ошибка 404 Пользователь нет в базе");
+                                            buffer = Encoding.UTF8.GetBytes("Error404$");
                                             client.Send(buffer);
-                                            return;
+
                                         }
-                                        command = "Select from dbo.[user].login='"+ reception__string.Split('/')[2].Split('%')[0] + "'";
-                                        log__text = sql__connect(command, true, "token");
+                                        else
+                                        {
+                                            command = "Select * from users__bd  where logins ='" + reception__string.Split('/')[2] + "'"; //"'  [passwords] = '" + transform__password(reception__string.Split('/')[3]) + "'";
+                                            log__text = sql__connect(command, true, "token");
 
-                                         buffer = Encoding.UTF8.GetBytes(log__text);
-                                        client.Send(buffer);
-                                        break;
+                                            buffer = Encoding.UTF8.GetBytes(log__text);
+                                            client.Send(buffer);
+                                        }
+                                            break;
                           }
-                       }     
+                       }
 
+
+                            break; }
+
+                    case "News" :{
+
+                                switch(reception__string.Split('/')[1])
+                                {
+
+                                    case "get":
+                                    {
+                                        string command = "use Test " + '\n' + " Select * from News";
+                                        string JSON = sql__connect(command, true, "News");
+
+                                        buffer = Encoding.UTF8.GetBytes(JSON);
+                                        client.Send(buffer);
+                                    }
+                                    break;
+                                }
 
                             break; }
                     case "default": { break; }
@@ -158,7 +220,7 @@ namespace ServerMarch
         private void button1_Click(object sender, EventArgs e)
         {
             dat = DateTime.Now;
-            timer1.Enabled = true;
+       //     timer1.Enabled = true;
             richTextBox1.Text = "Server => Сервер запускается" + '\n';
             sql = new SqlConnection("Data Source=DESKTOP-VB2HVQL\\MYSQL_BD;Initial Catalog=Test;Integrated Security=True");
             richTextBox1.Text += "Server => Подключение к БД" + '\n';
@@ -171,8 +233,7 @@ namespace ServerMarch
             richTextBox1.Text += "Server => Успешно: порт 8080" + '\n';
             server.Bind(iPEnd);
             richTextBox1.Text += "Server => Сервер запущен" + '\n';
-            Thread newThread = new Thread(server__start);
-            newThread.Start();
+            server__start();
             
         }
 
